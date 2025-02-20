56f132c61d21c83086166a10d2f7f86e85498100
saad1409 <saadalibamora@gmail.com>
2025-02-20 23:29:09 +0530

    commit

--- login.js
+++ login.js
@@ -1,3 +1,13 @@
+VANTA.GLOBE({
+    el: "#your-element-selector",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
 let sub=()=>{
 
     let inpname=document.querySelector("#name").value
