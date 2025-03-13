9071b31475a13022e379bb0314df23164a2e52a8
saad1409 <saadalibamora@gmail.com>
2025-03-13 11:57:19 +0530

    commit

--- login.js
+++ login.js
@@ -1,3 +1,32 @@
-let sub=()=>{
+let login = () =>{
+    let inpname=document.querySelector("#name").value
+    let inppass=document.querySelector("#pass").value
+
+
+
+    let errname=document.querySelector("#errname")
+    let errpass=document.querySelector("#errpass")
+
+
+
+    if(inpname ==""){
+        errname.innerHTML="Please enter name"
+        return false 
+    }
+
+
+
+    if (inppass =="") {
+        errpass.innerHTML = "Please enter Password ";
+        return false
+    }
     
+
+    localStorage.setItem("Name", inpname)
+    localStorage.setItem("Password", inppass)
+
+
+    location.href="index.html"
+
+    return false
 }
